d79fdf6476957a18561cac05e16ff2f0a9039ec9
Sinclair Akoto <sinclairakoto@Sinclairs-MBP.lan>
2020-06-29 03:41:35 +0100

    basic setup to test firebase create, read, delete operations with flask

--- bame.py
+++ bame.py
@@ -18,8 +18,23 @@ db = firebase.database()
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def test():
-    return "Hello World"
+def basic():
+    if request.method == 'POST':
+        if request.form.get('submit') == 'add':
+            name = request.form.get('nameVal')
+            # this creates a collection called todo in DB
+            # it also adds the 'name' variable value in the collection
+            db.child("todo").push(name)
+            # line 30 & 31 is to set up the read / get db method
+            todo = db.child("todo").get() # set name of collection a variable
+            to = todo.val() # encase the collection variable with a new variable, this allows you to put data back into web with jinja
+            return render_template('index.html', t=to.values()) # .values() will print all the values in the 'to' object (t in jinja)
+        elif request.form.get('submit') == 'delete':
+            # db.child("todo").equal_to("foodie").remove()
+        # return render_template('index.html', t=to.values())
+            db.child("todo").remove() # this removes ALL database values
+        return render_template('index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
