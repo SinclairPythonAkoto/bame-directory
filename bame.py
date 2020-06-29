5587ae42483e4bd6cced4ce61f2c185c97c4ddf0
Sinclair Akoto <sinclairakoto@Sinclairs-MBP.lan>
2020-06-29 02:56:12 +0100

    flask & pyrebase added

---
+++ bame.py
@@ -0,0 +1,11 @@
+import pyrebase
+from flask import *
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def test():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.run(debug=True)
