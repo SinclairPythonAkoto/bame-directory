2dfe2112d270c9ef680f5b42e3cfe67542d2d266
Sinclair Akoto <sinclairakoto@Sinclairs-MBP.lan>
2020-06-29 03:10:10 +0100

    firebase initialized with web app; database set up ready for use

--- bame.py
+++ bame.py
@@ -1,6 +1,20 @@
 import pyrebase
 from flask import *
 
+config = {
+    "apiKey": "AIzaSyDKrW_cr5X6QRF_E4vA1phbQcP9urHMbdk",
+    "authDomain": "bame-c5ecc.firebaseapp.com",
+    "databaseURL": "https://bame-c5ecc.firebaseio.com",
+    "projectId": "bame-c5ecc",
+    "storageBucket": "bame-c5ecc.appspot.com",
+    "messagingSenderId": "87535704965",
+    "appId": "1:87535704965:web:5a2aaa61776041199242ec",
+    "measurementId": "G-TCKXXBR3F8"
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
