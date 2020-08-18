4cbf6f6bcd44a475584ac6dd0227847f588def4b
Sinclair Akoto <sinclairakoto@Sinclairs-MacBook-Pro.local>
2020-08-18 19:46:52 +0100

    template from w3schools.com added, & demo Firebase config added

---
+++ demo1/deom.py
@@ -0,0 +1,26 @@
+import pyrebase
+from flask import *
+
+app = Flask(__name__)
+config = {
+    "apiKey": "AIzaSyDzGHV_bWTwsZbdAGkiNj11sLZ9khKf43E",
+    "authDomain": "flaskfirebase-869f6.firebaseapp.com",
+    "databaseURL": "https://flaskfirebase-869f6.firebaseio.com",
+    "projectId": "flaskfirebase-869f6",
+    "storageBucket": "flaskfirebase-869f6.appspot.com",
+    "messagingSenderId": "716659971362",
+    "appId": "1:716659971362:web:258c468b0a8434467e817b",
+    "measurementId": "G-D0K2VY1HVT"
+}
+
+firebase = pyrebase.initialize_app(config)
+
+auth = firebase.auth()
+db = firebase.database()
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    return render_template("layout.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
